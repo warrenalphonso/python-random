314df70370aaddbda66ec6c7f187aaeb11923a8b
Warren Alphonso <warrenalphonso02@gmail.com>
2021-03-20 12:29:09 -0700

    Clean up Advent of Code

--- adventofcode/helpers.py
+++ adventofcode/helpers.py
@@ -4,10 +4,3 @@ def data(day: int, parser=str, sep="\n") -> list:
     with open(f"data/{day}.txt") as f:
         sections = f.read().rstrip().split(sep)
         return list(map(parser, sections))
-
-def first(iterable, default=None) -> object:
-    "Return first item in iterable, or default."
-    return next(iter(iterable), default)
-
-def quantify(iterable, pred=bool) -> int:
-    return sum(1 for i in iterable if pred(i))
